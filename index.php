586d36e5f3b0cb8048f32c9053ac8bc820f59701
ValerioViteritti <vviteritti@gmail.com>
2024-07-24 15:18:59 +0200

    feat: inseriti i dati dell'array nella table col foreach.

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
+<?php 
+
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+// var_dump($hotels[0])
+
+?>
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -19,6 +66,47 @@
     </head>
 
     <body>
+        <div class="container my-5">
+            <form action="index.php" method="get">
+                <input type="radio" name="parking" >Con il parcheggio
+                <input type="radio" name="rate1" >1
+                <input type="radio" name="rate2" >2
+                <input type="radio" name="rate3" >3
+                <input type="radio" name="rate4" >4
+                <input type="radio" name="rate5" >5
+                <button class="btn btn-primary">Filtra</button>
+
+            </form>
+        <table class="table">
+            <thead>
+            <tr>
+                <?php foreach ($hotels[0] as $title => $value): ?>
+                <th> <?php echo ucfirst($title) ?> </th>
+                <?php endforeach; ?>
+            </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel):?>
+                    <tr>
+                        
+                        <td> <?php echo $hotel['name'] ?> </td>
+                        <td> <?php echo $hotel['description'] ?> </td>
+                        <td class="text-center"> <?php echo $hotel['parking']? 'SI' : 'NO' ?> </td>
+                        <td class="text-center"> <?php echo $hotel['vote'] ?> </td>
+                        <td class="text-center"> <?php echo $hotel['distance_to_center'] . ' ' . 'km' ?> </td>
+                        
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+            
+            
+        </table>
+
+
+        </div>
+
+
         
     </body>
 </html>
